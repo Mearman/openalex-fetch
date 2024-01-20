8f06696e9450c8c992a3f3ddafd11e85108ca1db
Joseph Mearman <joseph@mearman.co.uk>
2024-01-20 22:23:37 +0000

    Add new files and export modules

---
+++ src/apis/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthorsApi';
+export * from './AutocompleteApi';
+export * from './ConceptsApi';
+export * from './FundersApi';
+export * from './InfoApi';
+export * from './InstitutionsApi';
+export * from './ListApi';
+export * from './NgramsApi';
+export * from './PeopleApi';
+export * from './PublishersApi';
+export * from './SingleApi';
+export * from './SourcesApi';
+export * from './WorksApi';
