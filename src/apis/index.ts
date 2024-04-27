3a724c0a82f3d8a1b19267b631dd4a9408d77677
Joseph Mearman <joseph@mearman.co.uk>
2024-04-27 18:42:23 +0100

    Refactor API exports in index.ts

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,16 +1,17 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './AuthorsApi';
-export * from './AutocompleteApi';
-export * from './ConceptsApi';
-export * from './FundersApi';
-export * from './InfoApi';
-export * from './InstitutionsApi';
-export * from './ListApi';
-export * from './NgramsApi';
-export * from './PeopleApi';
-export * from './PublishersApi';
-export * from './RandomApi';
-export * from './SingleApi';
-export * from './SourcesApi';
-export * from './WorksApi';
+export * as AuthorsApi from './AuthorsApi';
+export * as AutocompleteApi from './AutocompleteApi';
+export * as ConceptsApi from './ConceptsApi';
+export * as FundersApi from './FundersApi';
+export * as InfoApi from './InfoApi';
+export * as InstitutionsApi from './InstitutionsApi';
+export * as ListApi from './ListApi';
+export * as NgramsApi from './NgramsApi';
+export * as PeopleApi from './PeopleApi';
+export * as PublishersApi from './PublishersApi';
+export * as RandomApi from './RandomApi';
+export * as SingleApi from './SingleApi';
+export * as SourcesApi from './SourcesApi';
+export * as WorksApi from './WorksApi';
+
