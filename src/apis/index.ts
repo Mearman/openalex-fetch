5e8cd9c78c1e2751f969e1ba9ba1aa361a70c3a3
Joseph Mearman <joseph@mearman.co.uk>
2024-07-04 06:04:59 +0100

    chore: Update OpenAPI generator ignore rules and API exports

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,20 +1,18 @@
 /* tslint:disable */
 /* eslint-disable */
-export * as AuthorsApi from "./AuthorsApi";
-export * as AutocompleteApi from "./AutocompleteApi";
-export * as ConceptsApi from "./ConceptsApi";
-export * as FundersApi from "./FundersApi";
-export * as InfoApi from "./InfoApi";
-export * as InstitutionsApi from "./InstitutionsApi";
-export * as ListApi from "./ListApi";
-export * as MultipleApi from "./MultipleApi";
-export * as NgramsApi from "./NgramsApi";
-export * as OpenAlexApi from "./OpenAlexApi";
-export * as PeopleApi from "./PeopleApi";
-export * as PublishersApi from "./PublishersApi";
-export * as RandomApi from "./RandomApi";
-export * as SingleApi from "./SingleApi";
-export * as SourcesApi from "./SourcesApi";
-export * as TopicsApi from "./TopicsApi";
-export * as WorksApi from "./WorksApi";
-
+export * from './AuthorsApi';
+export * from './AutocompleteApi';
+export * from './ConceptsApi';
+export * from './FundersApi';
+export * from './InfoApi';
+export * from './InstitutionsApi';
+export * from './ListApi';
+export * from './MultipleApi';
+export * from './NgramsApi';
+export * from './PeopleApi';
+export * from './PublishersApi';
+export * from './RandomApi';
+export * from './SingleApi';
+export * from './SourcesApi';
+export * from './TopicsApi';
+export * from './WorksApi';
